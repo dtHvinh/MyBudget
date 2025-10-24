4e269b4d4252a3829a2009b45918c670450448f0
dtHvinh <dangthehienvinh662@gmail.com>
2025-10-24 14:35:21 +0700

    feat(wallets): impl get wallet details by id

---
+++ MyBudget/Endpoints/WalletEndpoints/GetWalletDetails/ById/Endpoint.cs
@@ -0,0 +1,40 @@
+﻿using FastEndpoints;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.EntityFrameworkCore;
+using MyBudget.Data;
+
+namespace MyBudget.Endpoints.WalletEndpoints.GetWalletDetails.ById;
+
+public class Endpoint(ApplicationDbContext context) : EndpointWithoutRequest<Results<Ok<GetWalletDetailsResponse>, NotFound>>
+{
+    private readonly ApplicationDbContext context = context;
+
+    public override void Configure()
+    {
+        Get("{id}");
+        Group<WalletGroup>();
+    }
+
+    public override async Task<Results<Ok<GetWalletDetailsResponse>, NotFound>> ExecuteAsync(CancellationToken ct)
+    {
+        var id = Route<int>("id");
+
+        GetWalletDetailsResponse? wallet = await context.Wallets
+            .Where(e => e.Id == id)
+            .Select(e => new GetWalletDetailsResponse()
+            {
+                Id = e.Id,
+                Name = e.Name,
+                Balance = e.Balance,
+                Currency = e.Currency,
+                Type = e.Type,
+                CreatedDate = e.CreatedDate
+            })
+            .FirstOrDefaultAsync(ct);
+
+        if (wallet is null)
+            return TypedResults.NotFound();
+
+        return TypedResults.Ok(wallet);
+    }
+}
---
+++ MyBudget/Endpoints/WalletEndpoints/GetWalletDetails/ById/Models.cs
@@ -0,0 +1,13 @@
+﻿using MyBudget.Models;
+
+namespace MyBudget.Endpoints.WalletEndpoints.GetWalletDetails.ById;
+
+public sealed class GetWalletDetailsResponse
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = default!;
+    public decimal Balance { get; set; }
+    public string Currency { get; set; } = default!;
+    public WalletType Type { get; set; }
+    public DateTimeOffset CreatedDate { get; set; }
+}

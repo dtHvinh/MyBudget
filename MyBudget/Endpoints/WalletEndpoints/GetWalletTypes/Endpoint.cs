55d8d02833deac32c6dc05ffd9300ca68c3d3bae
dtHvinh <dangthehienvinh662@gmail.com>
2025-10-25 11:05:18 +0700

    feat(wallets): impl get all wallet types

---
+++ MyBudget/Endpoints/WalletEndpoints/GetWalletTypes/Endpoint.cs
@@ -0,0 +1,33 @@
+﻿using FastEndpoints;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.EntityFrameworkCore;
+using MyBudget.Data;
+
+namespace MyBudget.Endpoints.WalletEndpoints.GetWalletTypes;
+
+public class Endpoint(ApplicationDbContext context) : EndpointWithoutRequest<Ok<List<GetWalletTypesResponse>>>
+{
+    private readonly ApplicationDbContext _context = context;
+
+    public override void Configure()
+    {
+        Get("types");
+        Description(b => b
+            .WithTags("Wallets")
+            .Produces<List<GetWalletTypesResponse>>(StatusCodes.Status200OK)
+            .WithSummary("Get Wallet Types")
+            .WithDescription("Retrieves a list of available wallet types."));
+        Group<WalletGroup>();
+    }
+
+    public override async Task<Ok<List<GetWalletTypesResponse>>> ExecuteAsync(CancellationToken ct)
+    {
+        var walletTypes = await _context.WalletTypes.Select(wt => new GetWalletTypesResponse
+        {
+            Id = wt.Id,
+            Name = wt.Name,
+        }).ToListAsync(ct);
+
+        return TypedResults.Ok(walletTypes);
+    }
+}
---
+++ MyBudget/Endpoints/WalletEndpoints/GetWalletTypes/Models.cs
@@ -0,0 +1,7 @@
+﻿namespace MyBudget.Endpoints.WalletEndpoints.GetWalletTypes;
+
+public sealed class GetWalletTypesResponse
+{
+    public int Id { get; set; }
+    public required string Name { get; set; }
+}

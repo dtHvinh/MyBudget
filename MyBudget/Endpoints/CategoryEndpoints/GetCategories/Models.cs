b0f28369935cc4824f4873bd298680a9143ecd7d
dtHvinh <dangthehienvinh662@gmail.com>
2025-10-25 14:21:40 +0700

    feat(categories): impl create category

---
+++ MyBudget/Endpoints/CategoryEndpoints/GetCategories/Models.cs
@@ -0,0 +1,12 @@
+﻿using MyBudget.Models;
+
+namespace MyBudget.Endpoints.CategoryEndpoints.GetCategories;
+
+public sealed class GetCategoriesResponse
+{
+    public required int Id { get; set; }
+    public required string Name { get; set; }
+    public required string Description { get; set; }
+    public required string? Icon { get; set; }
+    public required CategoryType Type { get; set; }
+}

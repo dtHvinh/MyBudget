e001f60c5d4139211ebb014fa796676703b080d1
dtHvinh <dangthehienvinh662@gmail.com>
2025-10-25 15:11:57 +0700

    feat(transactions): impl delete transaction

---
+++ MyBudget/Endpoints/TransactionEndpoints/DeleteTransaction/Endpoint.cs
@@ -0,0 +1,39 @@
+﻿using FastEndpoints;
+using Microsoft.AspNetCore.Http.HttpResults;
+using MyBudget.Data;
+using MyBudget.Models;
+
+namespace MyBudget.Endpoints.TransactionEndpoints.DeleteTransaction;
+
+public class Endpoint(ApplicationDbContext context) : EndpointWithoutRequest<Results<NoContent, NotFound, ProblemHttpResult>>
+{
+    private readonly ApplicationDbContext _context = context;
+
+    public override void Configure()
+    {
+        Delete("{id:int}");
+        Group<TransactionGroup>();
+    }
+
+    public override async Task<Results<NoContent, NotFound, ProblemHttpResult>> ExecuteAsync(CancellationToken ct)
+    {
+        Transaction? transaction = await _context.Transactions.FindAsync([Route<int>("id")], ct);
+
+        if (transaction is null)
+        {
+            return TypedResults.NotFound();
+        }
+
+        _context.Transactions.Remove(transaction);
+
+        try
+        {
+            await _context.SaveChangesAsync(ct);
+            return TypedResults.NoContent();
+        }
+        catch (Exception ex)
+        {
+            return TypedResults.Problem(detail: ex.Message);
+        }
+    }
+}

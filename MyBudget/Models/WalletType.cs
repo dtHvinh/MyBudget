5ac17bfdabebf692d66ff8847891456c91a66991
dtHvinh <dangthehienvinh662@gmail.com>
2025-10-25 11:12:50 +0700

    change(walletType): unique index for name column

--- MyBudget/Models/WalletType.cs
+++ MyBudget/Models/WalletType.cs
@@ -1,5 +1,8 @@
-﻿namespace MyBudget.Models;
+﻿using Microsoft.EntityFrameworkCore;
 
+namespace MyBudget.Models;
+
+[Index(nameof(Name), IsUnique = true)]
 public sealed class WalletType
 {
     public int Id { get; set; }
